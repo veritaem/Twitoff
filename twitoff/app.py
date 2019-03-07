016583eb980c24cd87cfc028c994c47b38b033e5
veritaem <dcharris776@gmail.com>
2019-03-06 19:56:22 -0500

    hw got stuff yay

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,6 +2,8 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .predict import predict_user
+from .twitter import add_or_update_user
 
 
 def create_app():
@@ -9,7 +11,6 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['ENV'] = config('ENV')
     DB.init_app(app)
 
     @app.route('/')
@@ -17,10 +18,37 @@ def create_app():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
 
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None):
+        message = ''
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = 'User {} successfully added!'.format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = 'Error adding {}: {}'.format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                               message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare():
+        user1, user2 = request.values['user1'], request.values['user2']
+        if user1 == user2:
+            return 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2,
+                                      request.values['tweet_text'])
+            return user1 if prediction else user2
+
+
     @app.route('/reset')
     def reset():
         DB.drop_all()
         DB.create_all()
-        return render_template('base.html', title='DB RESET!', users=[])
+        return render_template('base.html', title='DB Reset!', users=[])
 
     return app
